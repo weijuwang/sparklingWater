188e2ab8e91f6a02b14856475dacdf895a8e6185
Weiju Wang <weijuw@outlook.com>
2025-08-05 16:41:06 -0700

    MCTS UCT function

--- src/main/kotlin/MonteCarloTree.kt
+++ src/main/kotlin/MonteCarloTree.kt
@@ -1,11 +1,45 @@
+import kotlin.math.*
+
 /**
  *
  */
 data class MonteCarloTree(
+    /**
+     *
+     */
     val wins: Int = 0,
+
+    /**
+     *
+     */
     val playouts: Int = 0,
-    val branches: Map<Action, MonteCarloTree> = mapOf()
+
+    /**
+     *
+     */
+    val branches: Map<Action, MonteCarloTree> = mapOf(),
+
+    /**
+     *
+     */
+    val parent: MonteCarloTree? = null
 ) {
+    companion object {
+        /**
+         *
+         */
+        const val EXPLORATION_PARAMETER = 1.414
+    }
+
+    /**
+     *
+     */
+    private fun uct() =
+        wins.toDouble() /playouts + EXPLORATION_PARAMETER * sqrt(
+            ln(parent!!.playouts.toDouble())
+                    / playouts
+            )
+
     /**
      *
      */
